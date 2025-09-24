a529610a6f404dea6cb5e6370757526ec260d585
Alex Tey <150091558+teyalex@users.noreply.github.com>
2025-09-23 22:13:57 -0400

    Update Factiva queries.sql

--- Factiva queries.sql
+++ Factiva queries.sql
@@ -7,11 +7,12 @@ text, making it all easier to read. the queries are pasted into a Factiva search
 -- Repeating Faunalytics/Sentient study
 
 	la=en and hl=climate and sn=PUBLICATION NAME and wc>250
-		-- from here, I manually selected and downloaded the first 100 relevant articles
+		-- from here, I manually selected and downloaded the first 100 relevant articles for each publication
 
 -- Original landscape analysis
 
 	-- Query segments
+		-- these were used to assemble the full queries below
 
 		-- Language, major U.S. sources, date range, word count greater than 250 /
 		la=en and rst=tmnbus and date after 20210801 and date before 20250731 and wc>250
@@ -29,12 +30,12 @@ text, making it all easier to read. the queries are pasted into a Factiva search
 		((((diet* or consum* or “eat less” or “eating less” or “eat more” or “eating more”) near7 (meat or livestock or beef or plant-based or veg* or flexitarian or seafood or pescatarian or fish)) or (atleast2 vegan* or atleast2 vegetarian*)) and (climate or sustainab* or environment*))
 
 	-- Full queries
-		--these are the three searches we  made in the database
+		-- these are the three searches we  made in the database
 
 		-- Climate (only)
 		la=en and rst=tmnbus and date after 20210801 and date before 20250731 and wc>250 not (update or Briefing or summary or letters or correction* or Daybook or "market talk" or "roundup" or "press release" or "results announcement" or shortage) and (ns=GCLIMT and ((climate /n2/ chang*) or (climate /n2/ crisis) or (climate /n2/ impact) or "global warming" or ((atleast2 climate or atleast2 warming or atleast2 environment*) or hlp=carbon (neutrality or capture or emissions) or atleast2 greenhouse$ or atleast3 decarboni*)))
 
-		-- Climate + meat
+		-- Climate + meat/animal agriculture
 		la=en and rst=tmnbus and date after 20210801 and date before 20250731 and wc>250 not (update or Briefing or summary or letters or correction* or Daybook or "market talk" or "roundup" or "press release" or "results announcement" or shortage) and (ns=GCLIMT and ((climate /n2/ chang*) or (climate /n2/ crisis) or (climate /n2/ impact) or "global warming" or ((atleast2 climate or atleast2 warming or atleast2 environment*) or hlp=carbon (neutrality or capture or emissions) or atleast2 greenhouse$ or atleast3 decarboni*))) and (((meat or dairy or livestock or cow or cows or cattle or beef or sheep or lamb or chicken or pork) near3 (consum* or eat* or methane or carbon or CO2 or emissions)) or ((meat near2 industr*) or (livestock near2 industr*)) or ("animal agriculture" or "factory farm*"))
 
 		-- Climate + meat + dietary change
