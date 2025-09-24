ed75b75687cf895bfc5df9a35b8913b281d55d47
Alex Tey <150091558+teyalex@users.noreply.github.com>
2025-09-23 22:12:51 -0400

    Update Factiva queries.sql

--- Factiva queries.sql
+++ Factiva queries.sql
@@ -28,7 +28,8 @@ text, making it all easier to read. the queries are pasted into a Factiva search
 		-- Mention of dietary change
 		((((diet* or consum* or “eat less” or “eating less” or “eat more” or “eating more”) near7 (meat or livestock or beef or plant-based or veg* or flexitarian or seafood or pescatarian or fish)) or (atleast2 vegan* or atleast2 vegetarian*)) and (climate or sustainab* or environment*))
 
-	-- Constructed queries
+	-- Full queries
+		--these are the three searches we  made in the database
 
 		-- Climate (only)
 		la=en and rst=tmnbus and date after 20210801 and date before 20250731 and wc>250 not (update or Briefing or summary or letters or correction* or Daybook or "market talk" or "roundup" or "press release" or "results announcement" or shortage) and (ns=GCLIMT and ((climate /n2/ chang*) or (climate /n2/ crisis) or (climate /n2/ impact) or "global warming" or ((atleast2 climate or atleast2 warming or atleast2 environment*) or hlp=carbon (neutrality or capture or emissions) or atleast2 greenhouse$ or atleast3 decarboni*)))
